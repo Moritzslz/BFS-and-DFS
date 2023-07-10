19758cc6d1e11c515b2b7a9d03fec2c75e41c282
schultzm <schultzmoritz@gmail.com>
2023-07-10 16:08:47 +0200

    Graphen

--- src/gad/bfs/Graph.java
+++ src/gad/bfs/Graph.java
@@ -1,26 +1,46 @@
 package gad.bfs;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Graph {
 
-	public Graph() {
+	private ArrayList<Integer>[] adjacencyMatrix;
+	private int currentNodeId;
 
+	public Graph() {
+		adjacencyMatrix = new ArrayList[1000000];
+		currentNodeId = 0;
 	}
 
 	public int addNode() {
-		return 0;
+		adjacencyMatrix[currentNodeId] = new ArrayList<>();
+		currentNodeId++;
+		return currentNodeId - 1;
 	}
 
 	public Collection<Integer> getAllNodes() {
-		return null;
+		ArrayList<Integer> allNodes = new ArrayList<>();
+		for (int i = 0; i < adjacencyMatrix.length; i++) {
+			if (adjacencyMatrix[i] != null) {
+				allNodes.add(i);
+			}
+		}
+		return allNodes;
 	}
 
 	public Collection<Integer> getAllNeighbours(int id) {
-		return null;
+		ArrayList<Integer> neighbours = new ArrayList<>();
+		for (int i = 0; i < adjacencyMatrix[id].size(); i++) {
+			if (adjacencyMatrix[id].get(i) == 1) {
+				neighbours.add(i);
+			}
+		}
+		return neighbours;
 	}
 
 	public void addEdge(int idA, int idB) {
-
+		adjacencyMatrix[idA].set(idB, 1);
+		adjacencyMatrix[idB].set(idA, 1);
 	}
 }
