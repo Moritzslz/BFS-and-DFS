39a529700fb3e13f6042872a1001f0b9e6c06cd2
moritzslz <schultzmoritz@gmail.com>
2023-07-10 23:52:08 +0200

    ConnectedComponents

--- src/gad/bfs/ConnectedComponents.java
+++ src/gad/bfs/ConnectedComponents.java
@@ -1,7 +1,13 @@
 package gad.bfs;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class ConnectedComponents {
 	private BFS search;
+	private Set<Integer> visited;
 
 	public ConnectedComponents() {
 		this.search = new BFS();
@@ -12,6 +18,33 @@ public class ConnectedComponents {
 	}
 
 	public int countConnectedComponents(Graph g) {
-		return 0;
+		int count = 0; // Zähler für die Anzahl der Zusammenhangskomponenten
+		visited = new HashSet<>();
+
+		for (int node : g.getAllNodes()) {
+			if (!visited.contains(node)) {
+				bfs(g, node);
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	private void bfs(Graph graph, int startNode) {
+		Queue<Integer> queue = new LinkedList<>();
+		visited.add(startNode);
+		queue.add(startNode);
+
+		while (!queue.isEmpty()) {
+			int currentNode = queue.poll();
+
+			for (int neighbor : graph.getAllNeighbours(currentNode)) {
+				if (!visited.contains(neighbor)) {
+					visited.add(neighbor);
+					queue.add(neighbor);
+				}
+			}
+		}
 	}
 }
