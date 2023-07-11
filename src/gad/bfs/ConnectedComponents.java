9ade54bfc2a50cf0e430853092d6d2c8e486765b
schultzm <schultzmoritz@gmail.com>
2023-07-11 14:57:31 +0200

    ConnectedComponents

--- src/gad/bfs/ConnectedComponents.java
+++ src/gad/bfs/ConnectedComponents.java
@@ -14,28 +14,11 @@ public class ConnectedComponents {
 	}
 
 	public int countConnectedComponents(Graph g) {
-		// Tiefensuche
 		int numberOfConnectedComponents = 0;
-		Queue<Integer> queue = new LinkedList<>();
-		Set<Integer> visited = new HashSet<>();
-
-		queue.add(0);
-		visited.add(0);
-
-		while (!queue.isEmpty()) {
-			int currentNode = queue.poll();
-
-			ArrayList<Integer> neighbours = new ArrayList<>();
-			neighbours.addAll(g.getAllNeighbours(currentNode));
-			if (neighbours.get(0) != null && !visited.contains(neighbours.get(0))) {
-				Integer neighbour = neighbours.get(0);
-				if (!visited.contains(neighbour)) {
-					queue.add(neighbour);
-					visited.add(neighbour);
-				}
-			} else {
-				numberOfConnectedComponents++;
-			}
+		search.sssp(g, 0);
+		for (Integer node : g.getAllNodes()) {
+			Integer depth = search.getDepth(node);
+			numberOfConnectedComponents = Math.max(depth, numberOfConnectedComponents);
 		}
 		return numberOfConnectedComponents;
 	}
